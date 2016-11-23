cb2d611f919cf97e31d07d30c2ed2cff5b3dcd0f
rubenvandenbroecke <vandenbroeckeruben@gmail.com>
2016-11-23 09:43:10 +0100

    mapping

--- genetic/format_example_data.py
+++ genetic/format_example_data.py
@@ -1,14 +1,11 @@
 import csv
 
-
-
-
 if __name__ == '__main__':
     def run():
 
-        f = open('input_example_to_format(tem4012).csv','r')
+        f = open('input_example_to_format(tem4012).csv', 'r')
         tabletowrite = []
-        reader = csv.reader(f,delimiter=';')
+        reader = csv.reader(f, delimiter=';')
         count = 0
         last = ''
         rowintable = 0
@@ -16,8 +13,8 @@ if __name__ == '__main__':
         listofnames = []
         for row in reader:
             print(row)
-            rowintable+=1
-            if count>0:
+            rowintable += 1
+            if count > 0:
                 if row[3] == last:
                     tabletowrite[columnintable].append(row[6])
                 else:
@@ -25,31 +22,25 @@ if __name__ == '__main__':
                     rowintable = 0
                     columnintable += 1
                     tabletowrite.append([0])
-                    tabletowrite[columnintable][0]= row[6]
-
-
+                    tabletowrite[columnintable][0] = row[6]
 
             last = row[3]
-            count+=1
+            count += 1
             if count == 4013:
                 break
         print(tabletowrite)
-
 
         # print diene table in e nieuw, je mag weer kommatjes omzettn.
         g = open('formatted_example_average_waiting_time_data.csv', 'w')
         writer = csv.writer(g)
         writer.writerow(listofnames)
         for r in tabletowrite:
-            actuallisttowrite= []
+            actuallisttowrite = []
             for k in r:
-                actuallisttowrite.append(k.replace(',','.'))
-            #print(actuallisttowrite)
+                actuallisttowrite.append(k.replace(',', '.'))
+            # print(actuallisttowrite)
             writer.writerow(actuallisttowrite)
             print(len(actuallisttowrite))
 
-
-
 if __name__ == "__main__":
     run()
-
