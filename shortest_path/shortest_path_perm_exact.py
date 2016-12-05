aa4df294e8c785b40693f55fe8490bc675542f83
rubenvandenbroecke <vandenbroeckeruben@gmail.com>
2016-12-05 11:34:11 +0100

    shortest path

--- shortest_path/shortest_path_perm_exact.py
+++ shortest_path/shortest_path_perm_exact.py
@@ -5,6 +5,7 @@ import datetime
 from project_managament.progress_bar import print_progress
 from general_functions import get_day_type, round_to_5min
 from random import shuffle
+import ast
 
 attractions = ['JorisendeDraak', 'Baron1898', 'Droomvlucht', 'DeVliegendeHollander', 'Pirana', 'Python',
                'CarnavalFestival', 'Stoomcarrousel', 'FataMorgana', 'Kleuterhof', 'VogelRok', 'VillaVolta',
@@ -14,8 +15,11 @@ attractions = ['JorisendeDraak', 'Baron1898', 'Droomvlucht', 'DeVliegendeHolland
 
 response = raw_input("Please enter nr of attractions: ") or 5
 
-shuffle(attractions)
-attractions = attractions[:int(response)]
+st = datetime.datetime.now()
+# shuffle(attractions)
+# attractions = attractions[:int(response)]
+list_index = x = ast.literal_eval(response)
+attractions = list(attractions[i] for i in list(i - 1 for i in list_index))
 
 # Permutaties maken
 attr_perm = list(itertools.permutations(attractions))
@@ -40,28 +44,30 @@ dir_duurtijd_data = 'C:\\Users\\vande\\Dropbox\\Project Management\\Efteling Dat
 duurtijd_df = pd.read_csv(dir_duurtijd_data, index_col=0)
 
 # Read the waiting time data
-dir_wachttijd_data = 'C:\\Users\\vande\\Dropbox\\Project Management\\Efteling Data\\Merged\\efteling_data_from_2016-11-02_to_2016-11-21_with_day_types.csv'
+dir_wachttijd_data = 'C:\\Users\\vande\\Dropbox\\Project Management\\Efteling Data\\Merged\\efteling_data_from_2016-11-02_to_2016-11-27_with_day_types.csv'
 wachttijd_df = pd.read_csv(dir_wachttijd_data, index_col=0)
 # Change variable name of 'hour'
 wachttijd_df = wachttijd_df.rename(columns={'hour': 'time'})
 # Create new variable, for grouping purposes
 wachttijd_df['hour'] = [i[:2] for i in wachttijd_df['time']]
 # Group the data , day_type , name and hour
-grouped_data = wachttijd_df.groupby(['day_type', 'name', 'hour'], as_index=False).mean()
+grouped_data = wachttijd_df.groupby(['day_type', 'name', 'time'], as_index=False).mean()
+# grouped_data = wachttijd_df.groupby(['day_type', 'name', 'hour'], as_index=False).mean()
 
 print_progress(start, stop, prefix='Progress:', suffix='Complete', barLength=50)
 
 # Empty distance array
 attr_distances = []
 
+# Assign which day type today is
+day_type_today = get_day_type(datetime.date.today())
+
 for perm in attr_perm:
-    # sys.stdout.write('\r{0}/{1}'.format(start, stop))
+    sys.stdout.write('\r{0}/{1}'.format(start, stop))
     leave_time = now = datetime.datetime.now()
     # Assign start time
     start_time = now
     # print 'Start time of the visit: {0}'.format(start_time)
-    # Assign which day type today is
-    day_type_today = get_day_type(datetime.date.today())
 
     total_duration = 0
     # Zoek duration van Ingang naar eerste Attractie
@@ -71,7 +77,7 @@ for perm in attr_perm:
     try:
         waiting_time = grouped_data.loc[
             (grouped_data['day_type'] == day_type_today) & (grouped_data['name'] == perm[0]) & (
-                grouped_data['hour'] == round_to_5min(arr_time).strftime('%H'))]['waiting_time'].values[0]
+                grouped_data['time'] == round_to_5min(arr_time).strftime('%H:%M'))]['waiting_time'].values[0]
     except IndexError:
         waiting_time = 0
 
@@ -97,7 +103,7 @@ for perm in attr_perm:
             try:
                 waiting_time = grouped_data.loc[
                     (grouped_data['day_type'] == day_type_today) & (grouped_data['name'] == perm[count + 1]) & (
-                        grouped_data['hour'] == round_to_5min(arr_time).strftime('%H'))]['waiting_time'].values[0]
+                        grouped_data['time'] == round_to_5min(arr_time).strftime('%H:%M'))]['waiting_time'].values[0]
             except IndexError:
                 waiting_time = 0
 
@@ -122,7 +128,7 @@ for perm in attr_perm:
 
 # Find the shortest path
 min_perm = min(attr_distances, key=lambda x: x['duration'])
-
+print datetime.datetime.now() - st
 print 'Attractions:'
 print '\t{0}: Entrance'.format(min_perm['start'].strftime('%Hu%M'))
 for i in xrange(len(min_perm['permutation'])):
